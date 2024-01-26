31d181d9ddebaf2385f1486f01bcc20d2a030e13
Adam Smith <axfanwashere@gmail.com>
2024-01-25 18:15:22 -0700

    feat: add label

---
+++ src/components/Label/Label.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+
+
+import {
+  Label
+} from "./index";
+
+import { Checkbox } from "../ui/checkbox";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Components/Label",
+  component: Label,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof Label>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: () => {
+    return (
+    <div>
+        <div className="rn-flex rn-items-center rn-space-x-2">
+          <Checkbox id="terms" />
+          <Label htmlFor="terms">Accept terms and conditions</Label>
+        </div>
+      </div>
+    );
+  },
+};
