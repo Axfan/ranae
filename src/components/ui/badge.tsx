f011cc3ff78fadbae15a89c1f7fdecb53f8fc96b
Adam Smith <axfanwashere@gmail.com>
2024-01-25 19:03:46 -0700

    feat: add badge

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const badgeVariants = cva(
+  "rn-inline-flex rn-items-center rn-rounded-full rn-border rn-px-2.5 rn-py-0.5 rn-text-xs rn-font-semibold rn-transition-colors focus:rn-outline-none focus:rn-ring-2 focus:rn-ring-ring focus:rn-ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "rn-border-transparent rn-bg-primary rn-text-primary-foreground hover:rn-bg-primary/80",
+        secondary:
+          "rn-border-transparent rn-bg-secondary rn-text-secondary-foreground hover:rn-bg-secondary/80",
+        destructive:
+          "rn-border-transparent rn-bg-destructive rn-text-destructive-foreground hover:rn-bg-destructive/80",
+        outline: "rn-text-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import 'tailwindcss/tailwind.css'
 export * from '@/components/Accordion';
 export * from '@/components/AspectRatio';
 export * from '@/components/Avatar';
+export * from '@/components/Badge';
 export * from '@/components/Label'
 export * from '@/components/Table';
